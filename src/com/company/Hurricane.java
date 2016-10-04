70dcfbf565eb7b1714e81bc9fc6de42c1a902249
michaelplott <mike.plott80@gmail.com>
2016-10-04 11:20:27 -0400

    Add basic login functionality

---
+++ src/com/company/Hurricane.java
@@ -0,0 +1,22 @@
+package com.company;
+
+/**
+ * Created by michaelplott on 10/4/16.
+ */
+public class Hurricane {
+    enum Category {
+        ONE, TWO, THREE, FOUR, FIVE
+    }
+
+    String name;
+    String location;
+    Category category;
+    String image;
+
+    public Hurricane(String name, String location, Category category, String image) {
+        this.name = name;
+        this.location = location;
+        this.category = category;
+        this.image = image;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,63 @@
 package com.company;
 
+import spark.ModelAndView;
+import spark.Session;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
+        HashMap<String, User> users = new HashMap<>();
+        Spark.get(
+                "/",
+                (request, response) -> {
+                    Session session = request.session();
+                    String name = session.attribute("loginName");
+                    User user = users.get(name);
 
+                    HashMap m = new HashMap();
+                    if (user != null) {
+                        m.put("name", user.name);
+                    }
+                    return new ModelAndView(m, "home.html");
+                },
+                new MustacheTemplateEngine()
+        );
+
+        Spark.post(
+                "/login",
+                (request, response) ->  {
+                    String username = request.queryParams("loginName");
+                    String uPass = request.queryParams("password");
+
+                    User user = users.get(username);
+                    if (user == null) {
+                        user = new User(username, uPass);
+                        users.put(username, user);
+                    }
+                    else if (!uPass.equals(user.password)) {
+                        response.redirect("/");
+                        return null;
+                    }
+
+                    Session session = request.session();
+                    session.attribute("loginName", username);
+                    response.redirect("/");
+                    return null;
+                }
+        );
+
+        Spark.post(
+                "/logout",
+                (request, response) -> {
+                    Session session = request.session();
+                    session.invalidate();
+                    response.redirect("/");
+                    return null;
+                }
+        );
     }
 }
